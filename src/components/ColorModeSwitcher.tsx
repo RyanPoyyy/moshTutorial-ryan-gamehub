d0f9830533460840aef4d91b51ecddbb4db80512
Ryan Poy <113697457+RyanPoyyy@users.noreply.github.com>
2023-07-27 22:39:38 +0800

    added search bar

--- src/components/ColorModeSwitcher.tsx
+++ src/components/ColorModeSwitcher.tsx
@@ -9,7 +9,9 @@ const ColorModeSwitcher = () => {
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       />
-      <Text>{colorMode === "dark" ? "Dark" : "Light"} Mode </Text>
+      <Text whiteSpace="nowrap">
+        {colorMode === "dark" ? "Dark" : "Light"} Mode{" "}
+      </Text>
     </HStack>
   );
 };
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,10 +2,12 @@ import { HStack, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import logo from "../assets/steam-logo.png";
 import ColorModeSwitcher from "./ColorModeSwitcher";
+import { SearchInput } from "./SearchInput";
 const NavBar = () => {
   return (
     <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="100px"></Image>
+      <SearchInput />
       <ColorModeSwitcher />
     </HStack>
   );
